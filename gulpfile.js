517d75b9215c5db29ef3da846ad45cbfe7a3f676
Matheus Naldi <matheus_naldi@hotmail.com>
2017-04-17 10:10:46 -0300

    Added pace-progress package and babelify for js in gulp
    - Added .htaccess file denying public folders listing
    - Added pace-progress package
    - Configured gulpfile.js
    - Added eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,45 @@
 const gulp                 = require('gulp');
 const $                    = require('gulp-load-plugins')();
+const eslintify            = require('eslintify');
+const babelify             = require('babelify');
+const env                  = require('dotenv').load().parsed;
 const LessPluginAutoPrefix = require('less-plugin-autoprefix');
 
 const autoprefix = new LessPluginAutoPrefix({
   browsers: ['last 2 versions']
+});
+
+gulp.task('browserify', function () {
+  gulp.src('src/js/app.js', {
+    read: false
+  })
+    .pipe($.bro({
+      transform: [
+        [eslintify, {'quiet-ignored': true}], babelify,
+      ],
+      error: $.notify.onError('Error: <%= error.message %>'),
+      debug: true
+    }))
+    .pipe($.plumber({
+      errorHandler: $.notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(gulp.dest('public/js'))
+    .pipe($.rename({
+      suffix: '.min'
+    }))
+    .pipe($.sourcemaps.init({
+      identityMap: true,
+      loadMaps: true,
+      debug: true
+    }))
+    .pipe($.uglify())
+    .pipe($.sourcemaps.write('./', {
+      sourceMappingURL: function(file) {
+        return env.APP_URL.replace(/http(s)?:/, '') + '/js/' + file.relative + '.map?v=' + String(Number(new Date));
+      }
+    }))
+    .pipe(gulp.dest('public/js'))
+    .pipe($.notify('Build de Javascript finalizada'));
 });
 
 gulp.task('css', function () {
@@ -23,6 +59,13 @@ gulp.task('css', function () {
     .pipe($.notify('Build de CSS finalizada'));
 });
 
-gulp.task('watch', ['css'], function () {
+gulp.task('lint', function () {
+  return gulp.src(['gulpfile.js', 'src/js/**/*.js'])
+    .pipe($.eslint())
+    .pipe($.eslint.format());
+});
+
+gulp.task('watch', ['css', 'browserify'], function () {
   gulp.watch('src/less/**/*.less', ['css']);
+  gulp.watch('src/js/**/*.js', ['browserify']);
 });
--- public/index.php
+++ public/index.php
@@ -5,9 +5,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no">
   <title>Matheus Naldi - Desenvolvedor Web</title>
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo './css/style.min.css?v=' . filemtime('./css/style.min.css') ?>" />
 </head>
 <body>
-AFIHUOFSJAHFGSAHJASH
+  Iniciando projeto
+  <script src="<?php echo './js/app.min.js?v=' . filemtime('./js/app.min.js') ?>"></script>
 </body>
 </html>
